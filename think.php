6376a2e7ffb09ab0d9af5daf16ba6d379b88c9aa
sunkang <13127955213@qq.com>
2023-05-22 17:09:01 +0800

    Update think.php

--- think.php
+++ think.php
@@ -1,5 +1,16 @@
 <?php 
-
+/**
+* 获取远程URL内容
+*/
+function get_remote_file($url,$is_json = false){
+    $client = guzzle_http();
+    $res    = $client->request('GET', $url);
+    $res =  (string)$res->getBody(); 
+    if($is_json){
+        $res = json_decode($res,true);
+    } 
+    return $res;
+}
 /**
 * 下载远程文件
 */
@@ -7,9 +18,7 @@ function download_remote_file($url,$path='',$name = ''){
     $name = $name?:'/uploads/tmp/'.md5($url).'.'.get_ext($url);
     $path = $path?:WWW_PATH;
     $file = $path.$name; 
-    $client = guzzle_http();
-    $res    = $client->request('GET', $url);
-    $context =  (string)$res->getBody();  
+    $context = get_remote_file($url);
     if(!file_exists($file)){  
         $dir = get_dir($file); 
         if(!is_dir($dir)){
