9b89a7a2bf0f727d8121a53f3292a4d29c246c1b
sunkang <13127955213@qq.com>
2023-05-30 11:45:07 +0800

    Update think.php

--- think.php
+++ think.php
@@ -1,5 +1,40 @@
 <?php 
 /**
+* 取后缀
+*/
+function get_ext_by_url($url){
+    $mime = lib\Mime::load();  
+    $type = get_mime($url);
+    if($type){
+        foreach($mime as $k=>$v){
+            if(is_array($v)){
+                if(in_array($type,$v)){
+                    $find = $k;
+                    break;
+                }
+            }else{
+                if($v == $type){
+                    $find = $k;
+                    break;
+                }
+            }
+        }
+    }    
+    return $find;
+}
+/**
+* 取mime
+*/
+function get_mime($url)
+{ 
+    if(strpos($url,'://')!==false){
+        $type = get_headers($url,true)['Content-Type'];  
+    }else{
+        $type = mime_content_type($url); 
+    }
+    return $type;
+}
+/**
 * 获取远程URL内容
 */
 function get_remote_file($url,$is_json = false){
@@ -35,7 +70,7 @@ function download_file($url,$contain_http = false){
 * 下载远程文件
 */
 function download_remote_file($url,$path='',$name = ''){
-    $name = $name?:'/uploads/tmp/'.date("Y-m-d").'/'.md5($url).'.'.get_ext($url);
+    $name = $name?:'/uploads/tmp/'.date("Y-m-d").'/'.md5($url).'.'.get_ext_by_url($url);
     $path = $path?:WWW_PATH;
     $file = $path.$name;  
     if(!file_exists($file)){ 
