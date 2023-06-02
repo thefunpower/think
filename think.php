286eb26677ad1cee40270c4d6e575771c92e9233
sunkang <13127955213@qq.com>
2023-06-02 11:41:49 +0800

    data

---
+++ think.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+* Copyright (c) 2021-2031, All rights reserved.
+* MIT LICENSE
+*/ 
+/**
+* 设置允许字段
+*/
+function data_allow($collect,$field){
+	global $_data_allow;
+	$_data_allow[$collect] = $field;
+}
+/**
+*返回允许字段值
+*/
+function get_data_allow($collect,$input)
+{
+	 global $_data_allow;
+	 $new_input;
+	 $field = $_data_allow[$collect];
+	 foreach($input as $k=>$v){
+	 	if(in_array($k,$field)){
+	 		$new_input[$k] = $v;
+	 	}
+	 }
+	 return $new_input;
+}
