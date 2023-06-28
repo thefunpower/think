c90f23106c68d41a8c5342ed9d2e21639c594507
sunkang <13127955213@qq.com>
2023-06-28 14:31:42 +0800

    topthink/think-queue 需要独立安装

--- helper.php
+++ helper.php
@@ -862,10 +862,33 @@ if(!function_exists('echats')){
 }
 
 /**
-* 添加到队列
-* push_task('app\printer\job\Printer',['tt'=>11],'');
+* 设置允许字段
 */
-function push_task($job, $data = '', $queue = ''){
-    think\facade\Queue::push($job, $data , $queue );
+function set_field_allow($collect,$field){
+	global $_data_allow;
+	$_data_allow[$collect] = $field;
 }
-
+/**
+* 获取允许字段
+*/
+function get_field_allow($collect){
+	global $_data_allow;
+	$d = $_data_allow[$collect];
+	$d[] = 'id';
+	return $d;
+}
+/**
+*返回允许字段值
+*/
+function get_data_allow($collect,$input)
+{
+	 global $_data_allow;
+	 $new_input;
+	 $field = $_data_allow[$collect];
+	 foreach($input as $k=>$v){
+	 	if($k == 'id' || in_array($k,$field)){
+	 		$new_input[$k] = $v;
+	 	}
+	 }
+	 return $new_input;
+}
