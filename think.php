bcd39dcc3b3accde738b253b058468877aa9bf9f
sunkang <13127955213@qq.com>
2023-12-26 14:32:21 +0800

    Update think.php

--- think.php
+++ think.php
@@ -1,4 +1,42 @@
 <?php 
+
+/**
+ * 消息模板
+ */
+function get_template($name, $replace_arr = [])
+{
+    $file = PATH . '/template/' . $name . '.php';
+    if(!file_exists($file)) {
+        throw new Exception("模板文件不存在");
+    }
+    ob_start();
+    include $file;
+    $content = ob_get_contents();
+    ob_end_clean();
+    foreach($replace_arr as $k => $v) {
+        $content = str_replace("{" . $k . "}", $v, $content);
+    }
+    return $content;
+}
+
+/**
+ *  获取接口请求数据，如果存在将不发起接口请求
+ */
+function get_api_data($title, $name)
+{
+    return db_get_one("api_data", "*", ['title' => $title,'name' => $name]);
+}
+
+/**
+ * 记录接口请求数据
+ */
+function api_data_insert($data = [])
+{
+    $data = db_allow($data);
+    $data['created_at'] = now();
+    db_insert("api_data", $data);
+}
+
 /**
 * 添加到队列
 * "topthink/think-queue": "^3" 
